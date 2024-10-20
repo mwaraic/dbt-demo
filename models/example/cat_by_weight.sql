908986782be38d66d0558a1a1b8c7c61cba5af63
waraichmaaz@gmail.com <waraichmaaz@gmail.com>
2024-10-20 18:50:03 -0400

    first commit

---
+++ models/example/cat_by_weight.sql
@@ -0,0 +1,21 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+
+select category, sum(weight) as total_weight
+from {{ ref('books') }} 
+group by category
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
